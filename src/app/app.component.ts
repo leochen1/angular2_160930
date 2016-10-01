8f5c2fdbee7fd6ac3180b85ab51f8072b8fcb28e
leochen <leocmc0723@hotmail.com>
2016-10-01 17:04:26 +0800

    直接在 app.component.html 存取注入的 datasvc 元件

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,14 +11,8 @@ export class AppComponent {
   keyword: string = 'test';
 
   constructor(private datasvc: DataService){
-    this.data = datasvc.data;
-  }
 
-  doDeleteArticle(item){
-    this.datasvc.doDeleteArticle(item);
   }
-
-  data = [];
 
   doSearch(keyword: string) {
     this.keyword = keyword;
